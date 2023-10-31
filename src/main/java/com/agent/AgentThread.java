711a932872ff1be904a4fabed1984a8316a39730
leochanj <leochanj@node0.throughput.lumos-pg0.utah.cloudlab.us>
2023-10-30 19:23:53 -0600

    add ORM option

--- src/main/java/com/agent/AgentThread.java
+++ src/main/java/com/agent/AgentThread.java
@@ -68,8 +68,8 @@ public class AgentThread implements Runnable, MessageHandler {
     public static final String CLASSNAME = "travel.service.TravelServiceImpl";
     public static final String METHODNAME = "query";
     public static final String TESTTP = "io.opentelemetry.api.trace.Span.current().addEvent(\"[LUMOS] HELLO!!!!!!!!\");";
-
     public static final int TESTLINE = 158;
+
 
     public AgentThread(Instrumentation inst) {
         this.inst = inst;
@@ -322,7 +322,7 @@ public class AgentThread implements Runnable, MessageHandler {
 
         while(!LumosAgent.playGroundFlag);
         // A test of adding a tracepoint, then remove it...
-        if(sname.contains("ts-order-service")){
+        if(LumosAgent.ORMContextOn && sname.contains("ts-order-service")){
            playground(5);
         }
          /*
