f473bb8571864ebbffa60c7db9ca504be20120b9
leochanj105 <leocjy105@gmail.com>
2023-11-22 23:21:59 -0500

    remove timestamped

--- src/main/java/com/agent/LumosAgent.java
+++ src/main/java/com/agent/LumosAgent.java
@@ -430,18 +430,18 @@ public class LumosAgent {
                 }
             }
 
-            for (int i = 0; i < targetstmts.size(); i++) {
-                Stmt stmt = targetstmts.get(i);
-                LumosInstrumentation inst = targetInsts.get(i);
-                if (inst instanceof TimestampedInstrumentation) {
-                    List<Stmt> inserts = inst.addInsts();
-                    if (inserts.size() > 0) {
-                        CompileUtils.insertAt(units, stmt, inserts.get(0), true);
-                        inserts.remove(0);
-                        CompileUtils.insertAt(units, stmt, inserts, false);
-                    }
-                }
-            }
+            // for (int i = 0; i < targetstmts.size(); i++) {
+            //     Stmt stmt = targetstmts.get(i);
+            //     LumosInstrumentation inst = targetInsts.get(i);
+            //     if (inst instanceof TimestampedInstrumentation) {
+            //         List<Stmt> inserts = inst.addInsts();
+            //         if (inserts.size() > 0) {
+            //             CompileUtils.insertAt(units, stmt, inserts.get(0), true);
+            //             inserts.remove(0);
+            //             CompileUtils.insertAt(units, stmt, inserts, false);
+            //         }
+            //     }
+            // }
 
             scToCompile.add(sclass);
         }
